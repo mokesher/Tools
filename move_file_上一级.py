193891b6bbbda81a3cfda17a735e2e0e4854f9ec
Moke <33320332+mokesher@users.noreply.github.com>
2019-11-14 17:38:02 +0800

    Create move_file_上一级.py

---
+++ move_file_上一级.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+import os,sys,shutil,random
+
+
+def run(start_path, result_file):
+    path_list = os.listdir(start_path)
+    for i in path_list:
+        file = os.path.join(start_path,i)
+
+        if os.path.isfile(file):
+            if file == os.path.join(result_file,i):
+                continue
+            elif ".mp4" in i:
+                print(file)
+                try:
+                    shutil.move(file, result_file)
+                except shutil.Error:
+                    name = i.rsplit(".")[0]
+                    typ = i.rsplit(".")[-1]
+                    new_name = name + str(random.randint(0,99)) + "." + typ
+                    print("rename",new_name)
+                    new_file = os.path.join(start_path,new_name)
+                    os.rename(file, new_file)
+                    shutil.move(new_file, result_file)
+        else:
+            run(file, result_file)
+
+
+start_path = r"E:\wan"
+result_file = start_path
+run(start_path, result_file)
+
+
